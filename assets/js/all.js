e7f501d19c4d1336089b8943025dee380eca9120
Amber <melody19941231@gmail.com>
2022-08-13 21:20:04 +0800

    Update 2022-08-13T13:19:50.050Z

---
+++ assets/js/all.js
@@ -0,0 +1,25 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.mobile-show').click(function (e) {
+    e.preventDefault();
+    $('.header-menu').toggleClass('menu-show');
+  });
+  $(".top-icon").hover(function () {
+    $(".icon-top-show").css("visibility", "visible");
+  }, function () {
+    $(".icon-top-show").css("visibility", "hidden");
+  });
+  $(".top-icon").click(function (e) {
+    e.preventDefault();
+    $("html, body").animate({
+      scrollTop: 0
+    }, 600);
+  });
+  $(".reserve-icon").hover(function () {
+    $(".icon-reserve-show").css("visibility", "visible");
+  }, function () {
+    $(".icon-reserve-show").css("visibility", "hidden");
+  });
+});
+//# sourceMappingURL=all.js.map

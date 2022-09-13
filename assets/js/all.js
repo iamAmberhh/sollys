8c0220f9d11b4f905af66dc451184ae71067d4b1
Amber <melody19941231@gmail.com>
2022-09-13 19:52:23 +0800

    Update 2022-09-13T11:52:17.542Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -50,11 +50,11 @@ AOS.init({
   // offset (in px) from the original trigger point
   delay: 0,
   // values from 0 to 3000, with step 50ms
-  duration: 800,
+  duration: 1000,
   // values from 0 to 3000, with step 50ms
   easing: 'ease',
   // default easing for AOS animations
-  once: true,
+  once: false,
   // whether animation should happen only once - while scrolling down
   mirror: false,
   // whether elements should animate out while scrolling past them
